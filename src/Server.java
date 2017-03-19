fa646c3f265fbd1ce783e64709e554966a306b87
Akbar96 <khanakbar1819@gmail.com>
2017-03-19 00:14:35 -0400

    Imported utilities and made variables

--- src/Server.java
+++ src/Server.java
@@ -1,4 +1,15 @@
 
-public class Server {
+import java.io.*;
+import java.net.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Server extends JFrame{
+	
+	private JTextField userText; //The field where the user can input the text
+	private JTextArea chatWindow;//The JFram window where conversations among the clients and servers
+								// can be read.
+	private ObjectOutputStream output;
 
 }

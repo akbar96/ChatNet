8e9f26f462df44c862577a1552b1d3cd976a3a7d
Akbar96 <khanakbar1819@gmail.com>
2017-03-19 00:30:57 -0400

    Comments and documentation added

--- src/Server.java
+++ src/Server.java
@@ -1,3 +1,13 @@
+/*
+ * This program has been made by Akbar Khan for EECS 3214 Winter 2017
+ * The purpose of this program is to handle connections from clients
+ * at a server and provide the clients IP address and listening port numbers
+ * of the peers to establish TCP connections with other peers and enabling
+ * them to chat with each other.
+ * Certian components of the program have been adopted from thenewboston tutorials
+ * */
+
+
 
 import java.io.*;
 import java.net.*;
@@ -7,9 +17,12 @@ import javax.swing.*;
 
 public class Server extends JFrame{
 	
-	private JTextField userText; //The field where the user can input the text
-	private JTextArea chatWindow;//The JFram window where conversations among the clients and servers
-								// can be read.
-	private ObjectOutputStream output;
-
+	private JTextField userText; 	//The field where the user can input the text
+	private JTextArea chatWindow;	//The JFram window where conversations among the clients and servers
+									// can be read.
+	private ObjectOutputStream output; // Messages going out from the user
+	private ObjectInputStream input;	//Messages being received by the user
+	private ServerSocket server;	//The server
+	private Socket connection; //Contains connections via sockets
+	
 }

de052fb662c02324ca9007aae2657735f3396353
Akbar96 <khanakbar1819@gmail.com>
2017-03-20 22:33:09 -0400

    Major changes
    Removed the GUI setup because of time constraints.
    For future work, it should be initialized in the client thread
    
    showMessage is switched back to System.out.println
    
    finally from try catch finally is removed becasue the closing connection
    is handled by the ClientThread class now

--- src/Server.java
+++ src/Server.java
@@ -22,37 +22,37 @@ public class Server extends JFrame{
 									// can be read.
 //	private ObjectOutputStream output; // Messages going out from the user		Not useful in this program.
 //	private ObjectInputStream input;	//Messages being received by the user	A different io stream is used
-	private ServerSocket server;	//The server
-	private Socket connection; //Contains connections via sockets
+//	private ServerSocket server;	//The server
+//	private Socket connection; //Contains connections via sockets
 	
-	//Constructs the GUI for Server
-	public Server(){
-		super("ChatNet");	//Naming the service as ChatNet
-		userText = new JTextField();	//Initiates a chat feild
-		userText.setEditable(false);	//Set the boolean for chatbox to false. This is the case initially, 
-										//this prevents the user form typing when they are not connected to
-										//anyone
-		userText.addActionListener(		//Adding an action listener
-				new ActionListener(){	//As soon as a new action is detected
-					public void actionPerformed(ActionEvent event){ /*This method is enabled and it passes
-					 												this action as event to the sendMessage
-					 												method*/
-						sendMessage(event.getActionCommand());	//passing in the text typed in to the text field using.getActionCommand
-						userText.setText("");//This sets the user text field to nothing after the message has been sent.
-						
-					}
-					
-				}
-		);
-		add(userText, BorderLayout.NORTH); //Adding the field in the window
-		chatWindow = new JTextArea();	//creation of Main window with all messages displayed in this window
-		add(new JScrollPane(chatWindow));//The main window gets added to the screen using this command
-		setSize(400,300);	//Set the size of the window
-		setVisible(true);	//Sets the window to be visible
-		
-				
-		
-	}
+//	//Constructs the GUI for Server
+//	public Server(){
+//		super("ChatNet");	//Naming the service as ChatNet
+//		userText = new JTextField();	//Initiates a chat feild
+//		userText.setEditable(false);	//Set the boolean for chatbox to false. This is the case initially, 
+//										//this prevents the user form typing when they are not connected to
+//										//anyone
+//		userText.addActionListener(		//Adding an action listener
+//				new ActionListener(){	//As soon as a new action is detected
+//					public void actionPerformed(ActionEvent event){ /*This method is enabled and it passes
+//					 												this action as event to the sendMessage
+//					 												method*/
+//						sendMessage(event.getActionCommand());	//passing in the text typed in to the text field using.getActionCommand
+//						userText.setText("");//This sets the user text field to nothing after the message has been sent.
+//						
+//					}
+//					
+//				}
+//		);
+//		add(userText, BorderLayout.NORTH); //Adding the field in the window
+//		chatWindow = new JTextArea();	//creation of Main window with all messages displayed in this window
+//		add(new JScrollPane(chatWindow));//The main window gets added to the screen using this command
+//		setSize(400,300);	//Set the size of the window
+//		setVisible(true);	//Sets the window to be visible
+////		
+//				
+//		
+//	}
 	
 	static String[] playersList = new String[1000]; //Initializes the array to the capacity of a 1000 elements
 	static int i=0;	//Populates the array
@@ -77,23 +77,23 @@ public class Server extends JFrame{
             ServerSocket server = new ServerSocket(portNumber, 100); //The port number is set to 21910 and at
             													// one point of time only 100 people can wait
             													// at the port
-            showMessage("Waiting for someone to connect... \n");
+            System.out.println("Waiting for someone to connect... \n");
             while(true){ 										//infinite loop which always checks for connections
             	try{
             		Socket connection = server.accept();		//The connection gets accepted here
-            		showMessage("Now conencted to "+ connection.getInetAddress().getHostName());
+            		System.out.println("Now conencted to "+ connection.getInetAddress().getHostName());
             		new ClientThread(connection).start();		/* Once the connection is accepted, the server starts a thread
             													by calling the class CLient Thread which caters to new 
             													connections and gives them an id.*/
             	}
             	catch (EOFException eofException){
-            		showMessage("\n Server ended the connection! "); //Looks for errors
-            	}finally{
-            		closeConnection();
+            		System.out.println("\n Server ended the connection! "); //Looks for errors
+//            	}finally{
+//            		closeConnection();
             	}
             }
             
-        } catch (Exception e) {
+        }catch (Exception e) {
             System.out.println("Exception caught when trying to listen on port "
                 + portNumber + " or listening for a connection");
             System.out.println(e.getMessage());

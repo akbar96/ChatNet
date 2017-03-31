2d38edc1a1b7fbcfc271e1e7dc0fd968800e7935
Akbar96 <khanakbar1819@gmail.com>
2017-03-30 23:54:28 -0400

    Minor adjustments

--- src/ClientThread.java
+++ src/ClientThread.java
@@ -38,85 +38,90 @@ public class ClientThread extends Thread {
 	     											//out.println("\n Streams are now setup! \n");			Not to be used in this version       	
 	     
 	     user.ipAddress = in.readLine();
+	     user.hostname = in.readLine();
+	     user.portNumber = Integer.parseInt(in.readLine());
+	     
 	     String inputLine;							//Contains the messages received by the user to the server.
          
-         while ((inputLine = in.readLine()) != null) { 		//ensures that there some input to compute
-         	
-        	 if (inputLine.equals("JOIN")){				 //Checks if the client wishes to join
-         		
-        		 if(joined == false){ 								//Makes sure that the same client is not joining twice
-        			out.println("Enter your numeric ID: "); 		//Asks client for an id to address them with. This id must be numerical
-        			Server.playersList[Server.i] = in.readLine();	//id is served in the array of players	         		
-        			out.println("Your id is: " + Server.playersList[Server.i]);//ID is confirmed to the player
-	         		id = Server.playersList[Server.i];				//stored in the variable id
-	         		Server.i = Server.i + 1;						//This statement increments the counter and sets it up for the next client
-        			joined = true;									//Once the client has joins, the boolean joined is set to true so that they cannot join again
-         		
-        		 }else{
-         			
-        			 out.println("You have already joined"); 		//If the player tries to join without leaving, they are reminded that they are already connected
-         		}
-         	
-        	 }else if(inputLine.equals("LIST")){					//Checks if the command is list
-         		
-         		//out.println("Message received: LIST");
-         		String message = "";								//Initializes the string message as empty
-         		
-         		for(int a = 0; a < 1000; a++){
-         			
-         			if(Server.playersList[a]!= null){				//Avoids null entries from being printed
-         				message = message.concat(Server.playersList[a]);//Gets the list of players
-         				message = message.concat(", ");
-         			}
-         		}
-         		out.println(message);								//List of live players is printed
-         		
-         	}else if(inputLine.equals("LEAVE")){					//Checks if the player wants to leave
-         		
-         		for(int a = 0; a < 1000; a++){
-         				
-         			if(Server.playersList[a] == id){				//Looks for the player's id in the array playersList
-         					Server.playersList[a] = null;			//Once found, that element is set to null
-         					joined = false;							//The player's joined boolean is changed back to false allowing them join agian
-         					
-         				break;										//Stops from marking other players with the same id as null/left
-         				}
-         				
-         			}
-         		
-         		out.println("Closing Connections...");				//Displays the message that the connections are being closed.
-         		out.println("You are no longer connected to Server \nStreams closed. \nConnection closed \nGoodbye!");
-         		
-         		try{
-         			out.close();						//Closes the output stream
-         			in.close();							//Closes the input stream
-         			connection.close();					//Closes the socket
-         			System.out.println("Client has Left");//Lets the player know they have left
-         			break;
-         		}catch(IOException ioException){
-         			
-         		}
-         		
-         	}else if(inputLine.equals("CHAT")){
-         		if(Server.playersList == null){
-         			out.println("There is no one connected to the Server. Try again later when someone else is connected.");
-         		}
-         		else if(Server.playersList != null && full == false){
-         			out.println("The chat engine should be executed now...");
-         		}
-         		
-         	}else{
-         		
-         		out.println("Command Invalid. Please try again");//Addresses all other commands besides JION, LIST, and LEAVE
-         	}
-         }
+	    while(true){ 
+	     
+	    
+	         while ((inputLine = in.readLine()) != null) { 		//ensures that there some input to compute
+	         	
+	        	 if (inputLine.toUpperCase().equals("JOIN")){				 //Checks if the client wishes to join
+	         		
+	        		 if(joined == false){ 								//Makes sure that the same client is not joining twice
+	        			out.println("Enter your name: "); 		//Asks client for an id to address them with. This id must be numerical
+	        			String name = in.readLine();
+	        			out.println("Your id is: " + user.username);//ID is confirmed to the player
+		         		
+	        			joined = true;									//Once the client has joins, the boolean joined is set to true so that they cannot join again
+	         		
+	        		 }else{
+	         			
+	        			 out.println("You have already joined"); 		//If the player tries to join without leaving, they are reminded that they are already connected
+	         		}
+	         	
+	        	 }else if(inputLine.equals("LIST")){					//Checks if the command is list
+	         		
+	         		//out.println("Message received: LIST");
+	         		int numThreads = 0;								//Initialized the number of threads to 0
+	         		
+	         		for(int a = 0; a < server.activeThrd.size(); a++){
+	         			
+	         			if(server.activeThrd.get(a).joined == TRUE){				//Avoids null entries from being printed
+	         				message = message.concat(Server.playersList[a]);//Gets the list of players
+	         				message = message.concat(", ");
+	         			}
+	         		}
+	         		out.println(message);								//List of live players is printed
+	         		
+	         	}else if(inputLine.equals("LEAVE")){					//Checks if the player wants to leave
+	         		
+	         		for(int a = 0; a < 1000; a++){
+	         				
+	         			if(Server.playersList[a] == id){				//Looks for the player's id in the array playersList
+	         					Server.playersList[a] = null;			//Once found, that element is set to null
+	         					joined = false;							//The player's joined boolean is changed back to false allowing them join agian
+	         					
+	         				break;										//Stops from marking other players with the same id as null/left
+	         				}
+	         				
+	         			}
+	         		
+	         		out.println("Closing Connections...");				//Displays the message that the connections are being closed.
+	         		out.println("You are no longer connected to Server \nStreams closed. \nConnection closed \nGoodbye!");
+	         		
+	         		try{
+	         			out.close();						//Closes the output stream
+	         			in.close();							//Closes the input stream
+	         			connection.close();					//Closes the socket
+	         			System.out.println("Client has Left");//Lets the player know they have left
+	         			break;
+	         		}catch(IOException ioException){
+	         			
+	         		}
+	         		
+	         	}else if(inputLine.equals("CHAT")){
+	         		if(Server.playersList == null){
+	         			out.println("There is no one connected to the Server. Try again later when someone else is connected.");
+	         		}
+	         		else if(Server.playersList != null && full == false){
+	         			out.println("The chat engine should be executed now...");
+	         		}
+	         		
+	         	}else{
+	         		
+	         		out.println("Command Invalid. Please try again");//Addresses all other commands besides JION, LIST, and LEAVE
+	         	}
+	         }
 //         private void sendMessage(String message){		Not certain if this is the method to be used
 //        	 try{
 //        		 
 //        	 }catch(IOException ioException){
 //        		 
 //        	 }
-//         }
+         }
         
 		}catch(Exception e){//LOoks for exception
         	 
